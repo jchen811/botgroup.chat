647241e08d54b10201f71164eb40ecb55e459131
chenjie <chenjie@nrp-pet.cn>
2025-03-25 18:06:48 +0800

    fix:模型及apikey连接问题修复

--- src/config/aiCharacters.ts
+++ src/config/aiCharacters.ts
@@ -1,52 +1,52 @@
 // 首先定义模型配置
 export const modelConfigs = [
   {
-    model: "qwen-plus",
+    model: "qwen-plus", // 0
     apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
     baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
   },
   {
-    model: "deepseek-v3-241226",
-    apiKey: "ARK_API_KEY",
-    baseURL: "https://ark.cn-beijing.volces.com/api/v3"
+    model: "deepseek-v3", // 1
+    apiKey: "DASHSCOPE_API_KEY",
+    baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
   },
   {
-    model: "hunyuan-turbos-latest",
-    apiKey: "HUNYUAN_API_KEY1",
+    model: "hunyuan-turbos-latest", // 2
+    apiKey: "HUNYUAN_API_KEY",
     baseURL: "https://api.hunyuan.cloud.tencent.com/v1"
   },
   {
-    model: "doubao-1-5-lite-32k-250115",//豆包模型|火山引擎接入点（改成自己的）
+    model: "doubao-1-5-lite-32k-250115",//豆包模型|火山引擎接入点（改成自己的） 3
     apiKey: "ARK_API_KEY",
     baseURL: "https://ark.cn-beijing.volces.com/api/v3"
   },
   {
-    model: "ep-20250306223646-szzkw",//deepseekv火山引擎接入点（改成自己的）
-    apiKey: "ARK_API_KEY1",
+    model: "ep-20250306223646-szzkw",//deepseekv火山引擎接入点（改成自己的） 4
+    apiKey: "ARK_API_KEY",
     baseURL: "https://ark.cn-beijing.volces.com/api/v3"
   },
   {
-    model: "glm-4-air",
+    model: "glm-4-air", // 5
     apiKey: "GLM_API_KEY",
     baseURL: "https://open.bigmodel.cn/api/paas/v4/"
   },
   {
-    model: "qwen-turbo",//调度模型
+    model: "qwen-turbo",//调度模型 6
     apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
     baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
   },
   {
-    model: "deepseek-chat",
+    model: "deepseek-chat", // 7
     apiKey: "DEEPSEEK_API_KEY",
     baseURL: "https://api.deepseek.com/v1"
   },
   {
-    model: "moonshot-v1-8k",
+    model: "moonshot-v1-8k", // 8
     apiKey: "KIMI_API_KEY",
     baseURL: "https://api.moonshot.cn/v1"
   },
   {
-    model: "ernie-3.5-128k",
+    model: "ernie-3.5-128k", // 9
     apiKey: "BAIDU_API_KEY",
     baseURL: "https://qianfan.baidubce.com/v2"
   }
